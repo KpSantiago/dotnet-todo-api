0bae140d800a4bf594996397661ce92802031bd7
KpSantiago <santiago.kauajose11@gmail.com>
2024-10-27 11:33:43 -0300

    feat: add configurations and Context in Program file
    description:
    This commit will add Newtonsoft JSON serializer, Swagger configurations
    and add the correct context to the context mapper

--- Program.cs
+++ Program.cs
@@ -1,18 +1,42 @@
-using api_dotnet.Context;
+using api_dotnet.Domain.Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddDbContext<AgendaContext>(
+builder.Services.AddDbContext<TodoContext>(
     options => options.UseSqlServer(
         builder.Configuration.GetConnectionString("connection")
         )
 );
-builder.Services.AddControllers();
+
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+{
+    // Impede as referências dos ciclos de objetos
+    /* libs:
+    dotnet add package Microsoft.AspNetCore.Mvc.NewtonsoftJson
+    dotnet add package Newtonsoft.Json
+    */
+    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+});
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(s =>
+{
+    s.SwaggerDoc("v1", new OpenApiInfo() { Title = "Todo-List API", Version = "v1" });
+
+    // Configurando agrupamento de rotas
+    s.TagActionsBy(api =>
+    {
+        return new List<string>()
+        {
+            api.GroupName
+        };
+    });
+
+    s.DocInclusionPredicate((_, api) => true);
+});
 
 var app = builder.Build();
 
